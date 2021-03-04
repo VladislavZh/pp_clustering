c9d968f4950e4d82634fe1afdc325418c4ae12b4
VladislavZh <vlad18_97@mail.ru>
2021-03-04 23:15:00 +0500

    args

--- run.py
+++ run.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
                 print('Solution failed')
             continue
         if args.degenerate_eps:
-            if cluster_part < args.degenerate_eps/arg.n_clusters:
+            if cluster_part < args.degenerate_eps/args.n_clusters:
                 if args.verbose:
                     print("Degenerate solution")
                 continue
