d16a37f556d45d5a0b47e04442e827c1cfba062a
Seva Grabar <grabarseva@gmail.com>
2021-06-25 17:29:41 +0300

    purity calc, booking utils

--- utils/calc_purity.py
+++ utils/calc_purity.py
@@ -1,24 +1,24 @@
-from utils.metrics import purity
 import argparse
-import os
-import glob
-import pandas as pd
-import numpy as np
-import torch
 import collections
+import glob
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+
+from utils.metrics import purity
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, default="K2_C5")
 args = parser.parse_args()
-exp_folder = os.path.join("experiments", args.dataset) 
-experiments = glob.glob(exp_folder+"/exp_*")
+exp_folder = os.path.join("experiments", args.dataset)
+experiments = glob.glob(exp_folder + "/exp_*")
 
 
-methods = ["cohortney", "kmeans", "ap", "gmm"]
-#res_dict = dict.fromkeys(methods)
+methods = ["cohortney", "kmeans", "gmm"]
 res_dict = collections.defaultdict(dict)
 for m in methods:
-
     res_dict[m]["time"] = 0
     res_dict[m]["purities"] = []
 
@@ -26,7 +26,7 @@ for m in methods:
 # iterating over experiments resuls
 n_runs = 0
 for exp in experiments:
-    clusters = os.path.join(exp, "compareclusters.csv")
+    clusters = os.path.join(exp, "compare_clusters.csv")
     if os.path.exists(clusters):
         n_runs += 1
         df = pd.read_csv(clusters)
@@ -34,22 +34,17 @@ for exp in experiments:
         # cohortney
         labels = df["coh_cluster"].to_list()
         res_dict["cohortney"]["time"] += df["time"][0]
-        pur = purity(torch.FloatTensor(true_labels),torch.FloatTensor(labels))
+        pur = purity(torch.FloatTensor(true_labels), torch.FloatTensor(labels))
         res_dict["cohortney"]["purities"].append(pur)
         # kmeans
         labels = df["kmeans_clusters"].to_list()
         res_dict["kmeans"]["time"] += df["kmeans_time"][0]
-        pur = purity(torch.FloatTensor(true_labels),torch.FloatTensor(labels))
-        res_dict["kmeans"]["purities"].append(pur) 
-        # ap
-        labels = df["ap_clusters"].to_list()
-        res_dict["ap"]["time"] += df["ap_time"][0]
-        pur = purity(torch.FloatTensor(true_labels),torch.FloatTensor(labels))
-        res_dict["ap"]["purities"].append(pur)
+        pur = purity(torch.FloatTensor(true_labels), torch.FloatTensor(labels))
+        res_dict["kmeans"]["purities"].append(pur)
         # gmm
         labels = df["gmm_clusters"].to_list()
         res_dict["gmm"]["time"] += df["gmm_time"][0]
-        pur = purity(torch.FloatTensor(true_labels),torch.FloatTensor(labels))
+        pur = purity(torch.FloatTensor(true_labels), torch.FloatTensor(labels))
         res_dict["gmm"]["purities"].append(pur)
 
 print("dataset", args.dataset)
@@ -57,6 +52,6 @@ print("number of runs", n_runs)
 print("number of clusters", len(np.unique(np.array(true_labels))))
 for m in methods:
     print(m)
-    print("mean alg time", res_dict[m]["time"]/n_runs)
+    print("mean alg time", res_dict[m]["time"] / n_runs)
     print("mean purity", np.mean(np.array(res_dict[m]["purities"])))
     print("stdev purity", np.std(np.array(res_dict[m]["purities"])))
--- utils/vote_booking.py
+++ utils/vote_booking.py
@@ -1,36 +1,61 @@
-import pandas as pd
-import os 
-import glob
+import os
 
+import pandas as pd
 
 # iterate over diff columns experiments folders
-for i in range(0, 1):
-
-    devcl = pd.read_csv(os.path.join("experiments/booking","deviceclass", "exp_"+str(i),"compareclusters.csv"))
-    diffinout = pd.read_csv(os.path.join("experiments/booking","diffinout", "exp_"+str(i),"compareclusters.csv"))
-    diffcheckin = pd.read_csv(os.path.join("experiments/booking","diffcheckin", "exp_"+str(i),"compareclusters.csv"))
-    
+n_runs = 1
+for i in range(0, n_runs):
+    # df with columns to vote
+    devcl = pd.read_csv(
+        os.path.join(
+            "experiments/booking", "deviceclass", "exp_" + str(i), "compareclusters.csv"
+        )
+    )
+    diffinout = pd.read_csv(
+        os.path.join(
+            "experiments/booking", "diffinout", "exp_" + str(i), "compareclusters.csv"
+        )
+    )
+    diffcheckin = pd.read_csv(
+        os.path.join(
+            "experiments/booking", "diffcheckin", "exp_" + str(i), "compareclusters.csv"
+        )
+    )
+    # csv with results
     res_df = pd.DataFrame()
+    res_df["seqlength"] = devcl["seqlength"].values
     # ground truth
     res_df["cluster_id"] = devcl["cluster_id"].values
-    res_df["seqlength"] = devcl["seqlength"].values
-    # coh labels voting
+    # cohortney labels voting
     res_df["coh_cluster_1"] = devcl["coh_cluster"].values
     res_df["coh_cluster_2"] = diffinout["coh_cluster"].values
     res_df["coh_cluster_3"] = diffcheckin["coh_cluster"].values
-    res_df["time"] = devcl["time"].values + diffinout["time"].values + diffcheckin["time"].values
-    print(i)
+    res_df["time"] = (
+        devcl["time"].values + diffinout["time"].values + diffcheckin["time"].values
+    )
     # voting - inferred cluster is most frequent
-    res_df["coh_cluster"] = res_df[["coh_cluster_1", "coh_cluster_2", "coh_cluster_3"]].mode(axis=1).iloc[:,0]
-    methods = ["kmeans", "ap", "gmm"]
+    res_df["coh_cluster"] = (
+        res_df[["coh_cluster_1", "coh_cluster_2", "coh_cluster_3"]]
+        .mode(axis=1)
+        .iloc[:, 0]
+    )
+    # tsfresh features with standard cluster algos
+    methods = ["kmeans", "gmm"]
     for m in methods:
-        res_df[m+"_time"] = devcl[m+"_time"].values + diffinout[m+"_time"].values + diffcheckin[m+"_time"].values
-        res_df[m+"_cluster_1"] = devcl[m+"_clusters"].values
-        res_df[m+"_cluster_2"] = diffinout[m+"_clusters"].values
-        res_df[m+"_cluster_3"] = diffcheckin[m+"_clusters"].values
-        res_df[m+"_time"] = devcl[m+"_time"].values + diffinout[m+"_time"].values + diffcheckin[m+"_time"].values
-        res_df[m+"_clusters"] = res_df[[m+"_cluster_1", m+"_cluster_2", m+"_cluster_3"]].mode(axis=1).iloc[:,0]
-    
-    res_df.to_csv(os.path.join("experiments/booking", "exp_" + str(i), "compareclusters.csv"))
+        res_df[m + "_time"] = (
+            devcl[m + "_time"].values
+            + diffinout[m + "_time"].values
+            + diffcheckin[m + "_time"].values
+        )
+        res_df[m + "_cluster_1"] = devcl[m + "_clusters"].values
+        res_df[m + "_cluster_2"] = diffinout[m + "_clusters"].values
+        res_df[m + "_cluster_3"] = diffcheckin[m + "_clusters"].values
+        res_df[m + "_clusters"] = (
+            res_df[[m + "_cluster_1", m + "_cluster_2", m + "_cluster_3"]]
+            .mode(axis=1)
+            .iloc[:, 0]
+        )
 
-
+    res_df.to_csv(
+        os.path.join("experiments/booking", "exp_" + str(i), "compare_clusters.csv")
+    )
